49acd79f49c438a9df912078a8ba620d074903d4
Niloy Sikdar <niloysikdar30@gmail.com>
2021-11-06 03:56:40 +0530

    Refactored Rating Buttons

--- src/components/RatingSelect.svelte
+++ src/components/RatingSelect.svelte
@@ -2,6 +2,15 @@
     import { createEventDispatcher } from "svelte";
     const dispatch = createEventDispatcher();
 
+    let RatingData = [];
+
+    [...Array(10).keys()].map((number) =>
+        RatingData.push({
+            id: `num${number + 1}`,
+            value: number + 1,
+        })
+    );
+
     let selected = 10;
 
     const onChange = (e) => {
@@ -56,104 +65,16 @@
 </style>
 
 <ul class="rating">
-    <li>
-        <input
-            type="radio"
-            id="num1"
-            name="rating"
-            value="1"
-            on:change={onChange}
-            checked={selected === 1} />
-        <label for="num1">1</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num2"
-            name="rating"
-            value="2"
-            on:change={onChange}
-            checked={selected === 2} />
-        <label for="num2">2</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num3"
-            name="rating"
-            value="3"
-            on:change={onChange}
-            checked={selected === 3} />
-        <label for="num3">3</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num4"
-            name="rating"
-            value="4"
-            on:change={onChange}
-            checked={selected === 4} />
-        <label for="num4">4</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num5"
-            name="rating"
-            value="5"
-            on:change={onChange}
-            checked={selected === 5} />
-        <label for="num5">5</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num6"
-            name="rating"
-            value="6"
-            on:change={onChange}
-            checked={selected === 6} />
-        <label for="num6">6</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num7"
-            name="rating"
-            value="7"
-            on:change={onChange}
-            checked={selected === 7} />
-        <label for="num7">7</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num8"
-            name="rating"
-            value="8"
-            on:change={onChange}
-            checked={selected === 8} />
-        <label for="num8">8</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num9"
-            name="rating"
-            value="9"
-            on:change={onChange}
-            checked={selected === 9} />
-        <label for="num9">9</label>
-    </li>
-    <li>
-        <input
-            type="radio"
-            id="num10"
-            name="rating"
-            value="10"
-            on:change={onChange}
-            checked={selected === 10} />
-        <label for="num10">10</label>
-    </li>
+    {#each RatingData as item (item.value)}
+        <li>
+            <input
+                type="radio"
+                id={item.id}
+                name="rating"
+                value={item.value}
+                on:change={onChange}
+                checked={selected === item.value} />
+            <label for={item.id}>{item.value}</label>
+        </li>
+    {/each}
 </ul>
