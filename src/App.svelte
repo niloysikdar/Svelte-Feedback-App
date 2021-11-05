609b1798e38252c19ef844518228ed506ffeda52
Niloy Sikdar <niloysikdar30@gmail.com>
2021-11-05 22:27:48 +0530

    Added Dummy FeedbackData and looping each feedback

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
+	import FeedbackList from "./components/FeedbackList.svelte";
+	import { FeedbackData } from "./FeedbackData";
 </script>
 
 <main>
 	<h1>Hello</h1>
+	<FeedbackList feedbackData={FeedbackData} />
 </main>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let feedbackData = [];
+</script>
+
+<h2>Feedback List</h2>
+{#each feedbackData as data (data.id)}
+    <p>{data.id}: {data.text}</p>
+{/each}
